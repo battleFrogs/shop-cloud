16552415ab2978f6718e7d90fdd1f535b2fe787f
Gjc <15168264328@163.com>
2022-04-06 22:42:51 +0800

    修改sql文件和pdm相关

--- sql文件.sql
+++ sql文件.sql
@@ -12,5 +12,20 @@ create table user
     primary key (user_id)
 );
 
+drop table if exists goods;
 
+/*==============================================================*/
+/* Table: goods                                                 */
+/*==============================================================*/
+create table goods
+(
+    goods_id             bigint not null auto_increment comment '主键Id',
+    goods_name           varchar(100) comment '商品名称',
+    goods_img            varchar(200) comment '商品图片',
+    goods_description    varchar(500) comment '商品描述',
+    on_self              tinyint comment '是否上架',
+    primary key (goods_id)
+);
+
+alter table goods comment '商品';
 
