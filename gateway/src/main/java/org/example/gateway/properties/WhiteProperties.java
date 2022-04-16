17ae1113df3faf2153cc7d7ac0d58214a874c25c
Gjc <15168264328@163.com>
2022-04-16 20:33:57 +0800

    修改共享配置改为 扩展配置

--- gateway/src/main/java/org/example/gateway/properties/WhiteProperties.java
+++ gateway/src/main/java/org/example/gateway/properties/WhiteProperties.java
@@ -3,6 +3,7 @@ package org.example.gateway.properties;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.List;
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "white")
+@RefreshScope
 public class WhiteProperties {
 
     private List<String> address;
